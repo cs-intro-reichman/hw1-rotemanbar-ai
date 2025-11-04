cec759d5bbc9009e3216b73b212dc697ec93dddb
Rotem Anbar <rotemanbar@gmail.com>
2025-11-04 12:11:23 +0200

    fix: print 0:00 AM instead of 12:00 AM

--- TimeFormat.java
+++ TimeFormat.java
@@ -1,20 +1,23 @@
 // Represents the hh:mm time format using an AM/PM format. 
 public class TimeFormat {
-	public static void main(String[] args) {
-		
-		int hours = Integer.parseInt("" + args[0].charAt(0) + args[0].charAt(1));
-		int minutes = Integer.parseInt("" + args[0].charAt(3) + args[0].charAt(4));
-        String suffix = (hours < 12) ? "AM" : "PM";
-		hours = hours % 12;
-        if (hours == 0) {
-            hours = 12;
-        }
-		System.out.print(hours + ":");
-		 if (minutes < 10) {
-			System.out.print("0");
-		 }
-		System.out.print(minutes + " ");
-        System.out.println(suffix);
+    public static void main(String[] args) {
 
-	}
+        int hours = Integer.parseInt("" + args[0].charAt(0) + args[0].charAt(1));
+        int minutes = Integer.parseInt("" + args[0].charAt(3) + args[0].charAt(4));
+
+        String suffix = (hours < 12) ? "AM" : "PM";
+
+        if (hours == 0) {
+            hours = 0;
+        } else if (hours > 12) {
+            hours = hours - 12;
+        }
+
+        System.out.print(hours + ":");
+        if (minutes < 10) {
+            System.out.print("0");
+        }
+        System.out.print(minutes + " ");
+        System.out.println(suffix);
+    }
 }
